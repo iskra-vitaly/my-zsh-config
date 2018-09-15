94137d45732432a31f514741d27b8b98076b8f63
Iskra Vitaly <iskra.vitaly@gmail.com>
2018-09-15 15:17:47 +0300

    brew completions support

--- zshrc.zsh
+++ zshrc.zsh
@@ -42,6 +42,10 @@ zstyle ':completion:*' verbose true
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
 zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
 
+if type brew &>/dev/null; then
+  FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+fi
+
 # VCS_Info
 autoload -Uz vcs_info
 
